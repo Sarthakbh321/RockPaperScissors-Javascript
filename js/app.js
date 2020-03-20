326a60350b43679c4198b16aed29685d48911504
Sarthak <sarthakbh321@gmail.com>
2020-03-20 21:57:15 +0530

    added win and lose condition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//0 -- Rock     1 -- Paper      2 -- Scissors
+
 $(document).ready(function() {
 
     let player_score = 0;
@@ -16,9 +18,45 @@ $(document).ready(function() {
 
 
         $(".rock-button").click(function() {
-            
+            let answer = getRandomInt(3);
+
+            if(answer === 2) {
+                alert("YOU WON");
+            } else if(answer == 0) {
+                alert("TIE");
+            } else {
+                alert("YOU LOST!");
+            }
+        })
+
+        $(".paper-button").click(function() {
+            let answer = getRandomInt(3);
+
+            if(answer === 0) {
+                alert("YOU WON");
+            } else if(answer == 1) {
+                alert("TIE");
+            } else {
+                alert("YOU LOST!");
+            }
+        })
+
+        $(".scissor-button").click(function() {
+            let answer = getRandomInt(3);
+
+            if(answer === 1) {
+                alert("YOU WON");
+            } else if(answer == 2) {
+                alert("TIE");
+            } else {
+                alert("YOU LOST!");
+            }
         })
         
     })
 
 })
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
