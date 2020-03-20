205293f978d9e2aacdf6818c5d10eff8d6fd3b05
Sarthak <sarthakbh321@gmail.com>
2020-03-20 20:30:16 +0530

    added transition to play screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+
+    let player_score = 0;
+    let computer_score = 0;
+
+    $('.yes-button').click(function() {
+        
+
+        $(".player-score-value").text(player_score);
+        $(".computer-score-value").text(computer_score);
+
+        $(".starting-screen").fadeOut(1000, function() {
+            $(".heading").css("padding-bottom", "5%");
+            $(".play-section").fadeIn(1000);
+        });
+
+        
+
+    })
+
 })
