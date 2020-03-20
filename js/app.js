aa046efc1a484b8a3b8bc79a7839c3974b631fb1
Sarthak <sarthakbh321@gmail.com>
2020-03-20 22:50:32 +0530

    added up-down animation

--- js/app.js
+++ js/app.js
@@ -15,48 +15,82 @@ $(document).ready(function() {
             $(".heading").css("margin-bottom", "5%");
             $(".play-section").fadeIn(500);
         });
+    })
 
+    //if rock is selected
+    $(".rock-button").click(function() {
+        let answer = getRandomInt(3);
 
-        $(".rock-button").click(function() {
-            let answer = getRandomInt(3);
+        $(".hand").addClass("upDown-animation");
 
+        setTimeout(function() {
             if(answer === 2) {
                 alert("YOU WON");
+                $(".player-score-value").text(++player_score);
             } else if(answer == 0) {
                 alert("TIE");
             } else {
                 alert("YOU LOST!");
+                $(".computer-score-value").text(++computer_score);
             }
-        })
 
-        $(".paper-button").click(function() {
-            let answer = getRandomInt(3);
+            $(".hand").removeClass("upDown-animation");
+        }, 1000);
 
+        
+    })
+
+    //if paper is selected
+    $(".paper-button").click(function() {
+        let answer = getRandomInt(3);
+        
+        $(".hand").addClass("upDown-animation");
+
+        setTimeout(function() {
             if(answer === 0) {
                 alert("YOU WON");
+                $(".player-score-value").text(++player_score);
             } else if(answer == 1) {
                 alert("TIE");
             } else {
                 alert("YOU LOST!");
+                $(".computer-score-value").text(++computer_score);
             }
-        })
 
-        $(".scissor-button").click(function() {
-            let answer = getRandomInt(3);
+            $(".hand").removeClass("upDown-animation");
+        }, 1000);
+    })
 
+
+    //if scissor is selected
+    $(".scissor-button").click(function() {
+        let answer = getRandomInt(3);
+
+        $(".hand").addClass("upDown-animation");
+
+        setTimeout(function() {
             if(answer === 1) {
                 alert("YOU WON");
+                $(".player-score-value").text(++player_score);
             } else if(answer == 2) {
                 alert("TIE");
             } else {
                 alert("YOU LOST!");
+                $(".computer-score-value").text(++computer_score);
             }
-        })
+
+            $(".hand").removeClass("upDown-animation");
+        }, 1000);
+
         
     })
 
-})
+});
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function upDownAnimation() {
+    
 }
